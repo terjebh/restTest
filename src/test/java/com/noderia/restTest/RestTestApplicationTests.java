6857d946c06bfd16c60dea7ad7a9ab0c5ee60b65
terjebh <terje@itfakultetet.no>
2021-05-15 23:10:47 +0200

    Initial commit

---
+++ src/test/java/com/noderia/restTest/RestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noderia.restTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
